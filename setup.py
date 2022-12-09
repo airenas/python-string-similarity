08b4ced049da720185873500b4f75c83f069d2f7
Airenas Vaičiūnas <airenass@gmail.com>
2022-12-09 10:21:27 +0200

    Update setup.py - add version

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+import subprocess
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+git_version = subprocess.run(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE).stdout.decode(
+    'utf-8').strip()
+
 setuptools.setup(
     name="strsimpy",
-    version="0.2.1",
+    version="0.2" + git_version,
     description="A library implementing different string similarity and distance measures",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/luozhouyang/python-string-similarity",
-    author="ZhouYang Luo",
+    url="https://github.com/airenas/python-string-similarity",
+    author="ZhouYang Luo, Airenas Vaiciunas",
     author_email="zhouyang.luo@gmail.com",
     packages=setuptools.find_packages(),
     include_package_data=True,
